0bdaf2e6265d4281a4bda55fd4750c19f4a0ef3d
margo <mrgt8989@gmail.com>
2022-03-10 19:57:52 +0300

    Update Hello world.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 #include <iostream>
 #include <string>
 
-int
-main()
-{
-  std::string name; // name
-  std::cin >> name; // input
-  std::cout << "Hello world from " << name << std::endl;
+int main(){
+    std::string name; // .....
+    std::cin >> name; // Ввод данных
+    std::cout << "Hello world from " << name << std::endl;
 }
