b02edcefd63e6cdc3b3f456e5ca9dc5a94d35b0f
margo <mrgt8989@gmail.com>
2022-03-10 19:53:45 +0300

    Changed code style in cpp file

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string name; //name
-    std::cin >> name; // input
-    std::cout << "Hello world from " << name << std::endl;
+int
+main()
+{
+  std::string name; // name
+  std::cin >> name; // input
+  std::cout << "Hello world from " << name << std::endl;
 }
